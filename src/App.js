d688ea55ac1bc533ffee97c59e79aeb5bdb7a82f
Ekaterina Varlamova <e.varlamova@dunice.net>
2021-03-10 21:02:26 +0300

    set router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Dishes from './pages/Dishes';
+import Ingredients from './pages/Ingredients';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' component={Dishes} exact />
+          <Route path='/:id' component={Ingredients} exact />
+          <Redirect to='/' />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
